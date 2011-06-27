17c2d4ec21ce65d328365a6f3bf233c5297b7502
Luke Redpath <luke@lukeredpath.co.uk>
2011-06-28 00:21:04 +0100

    Handle the correct status code for the S3 response.

--- lib/github/s3_uploader.rb
+++ lib/github/s3_uploader.rb
@@ -3,23 +3,21 @@ require 'restclient'
 module Github
   class S3Uploader 
     def upload(path, metadata)
-      RestClient.post(metadata["s3_url"], [
+      response = RestClient.post(metadata["s3_url"], [
         ["key", "#{metadata["prefix"]}#{metadata["name"]}"],
         ["acl", metadata["acl"]],
-        ["success_action_redirect", metadata["redirect"]],
+        ["success_action_status", 201],
         ["Filename", metadata["name"]],
         ["AWSAccessKeyId", metadata["accesskeyid"]],
         ["Policy", metadata["policy"]],
         ["Signature", metadata["signature"]],
         ["Content-Type", metadata["mime_type"]],
         ["file", File.open(path)]
-      ]) do |response, request, result, &block|
-        if response.code == 303
-          # it worked, but don't follow the redirect
-          return metadata["url"]
-        else
-          response.return!
-        end
+      ])
+      if response.code == 201
+        metadata["url"]
+      else 
+        false
       end
     end
   end
